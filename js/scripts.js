97261f2dd834e8363c43014385bf8731c959a47b
Chris Winter <wntrch55@gmail.com>
2023-03-02 03:39:41 -0700

    Updated header, top navbar, side navbar, and search box style and functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
-/* Set the width of the side navigation to 250px */
+/*When opening the side navbar, set the width to 250px and the right margin of the main content to 250px */
 function openNav() {
-	document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
-	document.getElementById("mySidenav").style.width = "0";
-  }
+	document.getElementById("categorySidenav").style.width = "250px";
+	document.getElementById("main").style.marginRight = "250px";
+}
+
+/*When closing the side navbar, set the width to 0 and the right margin of the main content back to 0 */
+function closeNav() {
+	document.getElementById("categorySidenav").style.width = "0";
+	document.getElementById("main").style.marginRight = "0";
+}
